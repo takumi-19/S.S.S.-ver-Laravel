aa81e7c4201d17c8853141f2e61d3d1aac90ea6f
takumi-19 <tkm19.k.univ@icloud.com>
2020-09-01 13:52:46 +0900

    set route of stadium

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('stadia/index');
-});
+Route::get('/', 'StadiumController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('stadium', 'StadiumController', ['only' => ['index']]);
