bd9c6a0f1e6e1a03edd828203e7994d1e638f43d
takumi-19 <tkm19.k.univ@icloud.com>
2020-09-04 15:02:28 +0900

    Update StadiumController.php

--- app/Http/Controllers/StadiumController.php
+++ app/Http/Controllers/StadiumController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Stadium;
+use App\Models\Team;
 
 class StadiumController extends Controller
 {
-    //
+    public function index () 
+    {
+        $stadia = Stadium::with('matches')->get();
+        $teams = Team::all();
+        $param = ['stadia' => $stadia, 'teams' => $teams];
+        return view('stadia/index', $param);
+    }
 }
