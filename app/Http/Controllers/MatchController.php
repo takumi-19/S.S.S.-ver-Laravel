0dd50540f327c23bd7122832b0759619316ad194
takumi-19 <tkm19.k.univ@icloud.com>
2020-09-12 15:20:14 +0900

    Create match/show page

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Match;
 use App\Models\Team;
+use App\Models\Stadium;
 
 class MatchController extends Controller
 {
-    public function show ()
+    public function show($id)
     {
+        $match = Match::findOrFail($id);
         $teams = Team::all();
-        $param = ['teams' => $teams];
+        $stadia = Stadium::all();
+        $param = ['match' => $match, 'teams' => $teams, 'stadia' => $stadia];
         return view('matches/show', $param);
     }
 }
-
--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -2,5 +2,37 @@
 
 @section('container')
 <div class="MatchShow">
+  <div class="MatchTitle">
+    <div class="MatchTitle__matchName">
+      明治安田生命Ｊ１リーグ 第{{ $match->week }}節
+    </div>
+    <div class="MatchTitle__stadiumName">
+    {{ $match->stadium->stadium_name }}
+    </div>
+  </div>
+  <div class="MatchCard">
+    <a href="#" class="HomeTeam Btn">
+      <div class="Btn__text">
+        HOME TEAM
+      </div>
+      <div class="Btn__teamLogo">
+        <img src={{ asset($match->home_team->team_logo) }} alt="ホームチームエンブレム" class="Btn__teamLogo--home">
+      </div>
+      <div class="Btn__teamName">
+        {{ $match->home_team->team_name }}
+      </div>
+    </a>
+    <a href="#" class="AwayTeam Btn">
+      <div class="Btn__text">
+        AWAY TEAM
+      </div>
+      <div class="Btn__teamLogo">
+        <img src={{ asset($match->away_team->team_logo) }} alt="アウェーチームエンブレム" class="Btn__teamLogo--away">
+      </div>
+      <div class="Btn__teamName">
+        {{ $match->away_team->team_name }}
+      </div>
+    </a>
+  </div>
 </div>
 @endsection
